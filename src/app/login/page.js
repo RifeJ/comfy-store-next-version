2adece59bb1b305d9c5db2bfc313a243cb8da246
abbosbek <abbosbekxanifa1917@gmail.com>
2026-06-29 16:23:43 +0500

    login/fix

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -8,7 +8,12 @@ import { toast } from "react-toastify";
 
 function Login() {
   const navigate = useRouter();
-  const [theme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") || "light";
+    }
+    return "light";
+  });
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
